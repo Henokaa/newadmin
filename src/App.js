fc87dc2adcdd3afd5c20a9b5b5c250918ba6d834
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-06-21 23:10:51 +0300

    Jwt auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 
 import Application from './Application'
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Login from './pages/Login';
 import { LoginContext } from "./Contexts/LoginContext"
 
-
+import { Provider } from 'react-redux';
+import store from './store';
+// import { loadUser } from './actions/authActions';
 function App() {
   const [showProfile, setShowProfile] = useState(false);
 
+  // useEffect(() => {
+  //   store.dispatch(loadUser());
+  //   })
   return (
+  <Provider store={store}>
   <LoginContext.Provider value = {{ setShowProfile, showProfile }}>
   <div className="App">
     {showProfile ? <Application/> : <Login />} 
   </div>
   </LoginContext.Provider>
+  </Provider>
   );
 }
 
